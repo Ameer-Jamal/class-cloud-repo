f1d7baef134b5fb7e24df739aac8a5769a2508d6
Ameer-Jamal <66119695+Ameer-Jamal@users.noreply.github.com>
2022-06-13 20:12:13 +0300

    This is the code worked from the flash drive with usbwebserver before admin and parent page implementation

---
+++ studentSignup.php
@@ -0,0 +1,19 @@
+<?php
+$conn = mysqli_connect('localhost', 'root', 'usbw', 'classclouddb');
+
+
+
+
+if (isset($_POST['password'])) {
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $grade = mysqli_real_escape_string($conn, $_POST['grade']);
+    $query = "INSERT INTO Students(name,email,password,grade) VALUES('$name','$email','$password','$grade')";
+}
+
+if (mysqli_query($conn, $query)) {
+    echo "added";
+} else {
+    echo 'ERROR ' . mysqli_error($conn);
+}
